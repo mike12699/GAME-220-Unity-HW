fbfb8bb432098cfcaeed7956d8be9b614e197189
mike12699 <335565@colum.edu>
2020-03-08 20:05:21 -0500

    Unity Project Chapter 2 Exercise 3

---
+++ Chapter 2, Exercise 3, Michael Nadasky/Assets/SphereMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+    public Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 100.0f;
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxis("Horizontal") * Time.deltaTime, Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+    void moveCharacter(Vector2 direction)
+    {
+        rb.AddForce(direction * speed);
+    }
+}
